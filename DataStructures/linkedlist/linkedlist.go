a0bb207e5cfb5ff2364bcc11d292172c5ddf211b
tabbakha, ahmad <tabba.ahmad@gmail.com>
2019-01-13 21:17:34 -0600

    add push and append to linked list, switch to go mod

--- DataStructures/linkedlist/linkedlist.go
+++ DataStructures/linkedlist/linkedlist.go
@@ -1,23 +1,49 @@
 package linkedlist
 
+import "fmt"
+
 // Node is a data structure which points to the next node in a linked list
 type Node struct {
-	Value interface{}
-	Next  *Node
+	data int
+	Next *Node
 }
 
 // List is a linked list
-type List struct {
-}
+type List struct{}
 
 // NewList returns a new list
-func NewList() {}
+func NewList() *Node {
+	return &Node{}
+}
 
-// Insert inserts item into given position
-func Insert() {}
+// Push to beginning of list
+func (head *Node) Push(d int) *Node {
+	return NewNode(d, head)
+}
+
+// Append to end of the list
+func (head *Node) Append(d int) {
+	for head.Next != nil {
+		head = head.Next
+	}
+	head.Next = NewNode(d, nil)
+}
 
 // Update updates item at given position
 func Update() {}
 
 // Delete deletes an item at given position
 func Delete() {}
+
+// NewNode create node
+func NewNode(d int, next *Node) *Node {
+	return &Node{data: d, Next: next}
+}
+
+// Print the list at the given node as head
+func Print(current *Node) {
+	for current != nil {
+		fmt.Println("Node: ", current.data)
+		current = current.Next
+	}
+}
--- DataStructures/linkedlist/linkedlist_test.go
+++ DataStructures/linkedlist/linkedlist_test.go
@@ -1,9 +1,25 @@
-package linkedlist
+package linkedlist_test
 
 import (
 	"testing"
+
+	"github.com/whatsadebugger/algorithms-data-structures/DataStructures/linkedlist"
 )
 
-func TestInsert(t *testing.T) {}
-func TestUpdate(t *testing.T) {}
-func TestDelete(t *testing.T) {}
+func TestLinkedList(t *testing.T) {
+	head := linkedlist.NewList()
+	head.Append(10)
+	head.Append(10)
+	head = head.Push(1)
+	linkedlist.Print(head)
+}
+
+func TestInsert(t *testing.T) {
+
+}
+func TestUpdate(t *testing.T) {
+
+}
+func TestDelete(t *testing.T) {
+
+}
